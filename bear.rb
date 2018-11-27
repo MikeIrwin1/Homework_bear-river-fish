940a00ad64669dd75e7702c2c9f084e99a9687b1
Michael Irwin <mike.irwin1@gmail.com>
2018-11-27 21:59:07 +0000

    bear roar test completed

--- bear.rb
+++ bear.rb
@@ -10,13 +10,17 @@ class Bear
   end
 
   def eat(fish_object, river)
-    for individual_fish in river[@fish]
+    for individual_fish in river.fish
       if individual_fish == fish_object
         @stomach << individual_fish
         river.fish.delete(individual_fish)
       end
     end
     return @count += 1
+  end
+
+  def roar
+    return "Roooaaaar"
   end
 
 end
